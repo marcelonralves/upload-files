4a353a7cd8c445d8c0797f0f0e4dd21507acdf56
Marcelo Alves <marcelonralves@gmail.com>
2022-03-30 18:52:05 -0300

    Estruturação do projeto

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request)
+    {
+
+    }
+
+    public function showUsers(Request $request)
+    {
+
+    }
+
+    public function editUser(Request $request)
+    {
+
+    }
+
+    public function removeUser(Request $request)
+    {
+
+    }
+}
